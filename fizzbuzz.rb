7da347c237265433d662ad3436caf3b44f85d469
Javier Soto <sotoseattle@gmail.com>
2014-09-15 19:38:14 -0700

    Interpolate instead of concatenate strings for performance.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,8 @@ def fizzbuzz(n)
   raise ArgumentError unless n.kind_of?(Fixnum)
 
   str = ""
-  str << "Fizz" if n%3==0
-  str << "Buzz" if n%5==0
+  str = "Fizz" if n%3==0
+  str = "#{str}Buzz" if n%5==0
   str = "#{n}" if str.empty?
 
   return str
