15d00ab302376427047a51e9a5c97b2617867122
Javier Soto <sotoseattle@gmail.com>
2014-09-22 12:50:58 -0700

    allow multiple modulus pairs

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,23 @@
-def fizzbuzz(n)
-  raise ArgumentError unless n.is_a?(Fixnum)
-  str = ""
-  str << "Fizz" if n % 3 == 0
-  str << "Buzz" if n % 5 == 0
-  str = "#{n}" if str.empty?
-  str
+class FizzBuzz
+  attr_reader :settings
+
+  def initialize(filename)
+    @settings = {}
+    File.open(filename).each_line do |line|
+      val, str  = line.strip.chomp.split("\s")
+      settings[val.to_i] = str
+    end
+  end
+
+  def fizzbuzz(n)
+    matches = settings.keys.sort.select { |k| n % k == 0 }
+    printer(matches, n)
+  end
+
+  private
+
+  def printer(matches, n)
+    return n if matches.empty?
+    matches.map { |k| settings[k] }.join
+  end
 end
--- spec/FizzBuzz_spec.rb
+++ spec/FizzBuzz_spec.rb
@@ -1,50 +1,29 @@
-require "rspec"
-require "./lib/fizzbuzz"
+require 'rspec'
+require './lib/fizzbuzz'
 
-RSpec.describe "FizzBuzz Method" do
+RSpec.describe 'FizzBuzz Method' do
+  let(:f) { FizzBuzz.new('./spec/whatever.txt') }
 
-  it "takes one number as argument" do
-    expect { fizzbuzz(42, 32) }.to raise_error
-    expect { fizzbuzz("hola") }.to raise_error
-    expect { fizzbuzz }.to raise_error
-    expect { fizzbuzz(42) }.not_to raise_error
+  it 'outputs Buzz when multiple of number' do
+    expect(f.fizzbuzz(15)).to eq('FizzBuzz')
+    expect(f.fizzbuzz(10)).to eq('Buzz')
+    expect(f.fizzbuzz(-7)).to eq("Sivv")
+    expect(f.fizzbuzz(3*5*7)).to eq('FizzBuzzSivv')
   end
 
-  it "outputs Fizz when multiple of 3" do
-    expect(fizzbuzz(9)).to eq("Fizz")
-    expect(fizzbuzz(9)).not_to eq("Buzz")
-    expect(fizzbuzz(9)).not_to eq("FizzBuzz")
-    expect(fizzbuzz(9)).not_to eq("9")
+  it 'outputs FizzBuzz for 0 too' do
+    expect(f.fizzbuzz(0)).to eq('FizzBuzzSivv')
+    expect(f.fizzbuzz(0)).not_to eq('Fizz')
+    expect(f.fizzbuzz(0)).not_to eq('Buzz')
+    expect(f.fizzbuzz(0)).not_to eq('0')
   end
 
-  it "outputs Buzz when multiple of 5" do
-    expect(fizzbuzz(10)).to eq("Buzz")
-    expect(fizzbuzz(10)).not_to eq("Fizz")
-    expect(fizzbuzz(10)).not_to eq("FizzBuzz")
-    expect(fizzbuzz(10)).not_to eq("10")
-  end
-
-  it "outputs FizzBuzz when multiple of 3 and 5" do
-    expect(fizzbuzz(30)).to eq("FizzBuzz")
-    expect(fizzbuzz(30)).not_to eq("Fizz")
-    expect(fizzbuzz(30)).not_to eq("Buzz")
-    expect(fizzbuzz(30)).not_to eq("30")
-  end
-
-  it "outputs FizzBuzz for 0 too" do
-    expect(fizzbuzz(0)).to eq("FizzBuzz")
-    expect(fizzbuzz(0)).not_to eq("Fizz")
-    expect(fizzbuzz(0)).not_to eq("Buzz")
-    expect(fizzbuzz(0)).not_to eq("0")
-  end
-
-  it "outputs the number in any other case" do
-    expect(fizzbuzz(2)).to eq("2")
-    expect(fizzbuzz(12234871)).to eq("12234871")
-    expect(fizzbuzz(-7)).to eq("-7")
-    expect(fizzbuzz(13)).to eq("13")
-    expect(fizzbuzz(13)).not_to eq("Fizz")
-    expect(fizzbuzz(13)).not_to eq("Buzz")
-    expect(fizzbuzz(13)).not_to eq("FizzBuzz")
+  it 'outputs the number in any other case' do
+    expect(f.fizzbuzz(2)).to eq(2)
+    expect(f.fizzbuzz(12_234_871)).to eq(12234871)
+    expect(f.fizzbuzz(13)).to eq(13)
+    expect(f.fizzbuzz(13)).not_to eq('Fizz')
+    expect(f.fizzbuzz(13)).not_to eq('Buzz')
+    expect(f.fizzbuzz(13)).not_to eq('FizzBuzz')
   end
 end
